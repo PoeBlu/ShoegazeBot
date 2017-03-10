ae67bc8397d0eda0f89d76f1b10d9bf9940455c5
Fernando Espinosa <fespinosa@ucdavis.edu>
2017-03-09 16:51:08 -0800

    Update markovgenerate.py

--- markovgenerate.py
+++ markovgenerate.py
@@ -9,6 +9,6 @@ with open("shoegaze_lyrics_bank.txt") as f:
 text_model = markovify.NewlineText(text)
 
 
-# Print three randomly-generated sentences of no more than 140 characters
+# Print three randomly-generated sentences of no more than 80 characters
 for i in range(3):
     print(text_model.make_short_sentence(80) + '\n')
